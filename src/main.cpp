e6338f602642516adfc315e37de1b00f52f77ee9
Romeu Vieira <romeu.bizz@gmail.com>
2020-04-12 00:27:57 +0100

    .debug_info section information

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,29 @@ static void DwarfErrorHandler(Dwarf_Error Error, Dwarf_Ptr ErrArg)
     exit(-1);
 }
 
+void DebugInfoSectionInfo(Dwarf_Debug* DwarfDebug)
+{
+    const char* ActualSectionName = 0;
+    Dwarf_Small IsMarkedCompressed = 0;
+    Dwarf_Small IsMarkedZLibCompressed = 0;
+    Dwarf_Small IsMarkedSHFCompressed = 0;
+    Dwarf_Unsigned CompressedLength = 0;
+    Dwarf_Unsigned UncompressedLength = 0;
+    Dwarf_Error DwarfError;
+
+    dwarf_get_real_section_name(*DwarfDebug, ".debug_info", &ActualSectionName,
+                                &IsMarkedCompressed, &IsMarkedZLibCompressed,
+                                &IsMarkedSHFCompressed, &CompressedLength,
+                                &UncompressedLength, &DwarfError);
+
+    fprintf(stdout, "ActualSectionName = %s\nIsMarkedCompressed = %d\n"
+                    "IsMarkedZLibCompressed = %d\nIsMarkedSHFCompressed = %d\n"
+                    "CompressedLength = %d\nUncompressedLength = %d\n",
+            ActualSectionName,
+            IsMarkedCompressed, IsMarkedZLibCompressed, IsMarkedSHFCompressed,
+            CompressedLength, UncompressedLength);
+}
+
 int main(void)
 {
     int DwarfResult = -1;
@@ -34,6 +57,8 @@ int main(void)
     dwarf_record_cmdline_options(DwarfOptions);
 
     fprintf(stdout, "DwarfDebug: %p\nDwarfError: %p\n", DwarfDebug, DwarfError);
+
+    DebugInfoSectionInfo(&DwarfDebug);
 
     DwarfResult = dwarf_finish(DwarfDebug, &DwarfError);
     if (DwarfResult != DW_DLV_OK) {
