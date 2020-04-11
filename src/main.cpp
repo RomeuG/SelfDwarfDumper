a1ea9ce034803840260b9e8671e0c32766c259ab
Romeu Vieira <romeu.bizz@gmail.com>
2020-04-12 00:02:24 +0100

    DwarfError Handler function

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,35 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
+// NOTE: only handled if Dwarf_Error == NULL in Dwarf functions
+static void DwarfErrorHandler(Dwarf_Error Error, Dwarf_Ptr ErrArg)
+{
+    fprintf(stderr, "Libdwarf error: %d - %s\n", dwarf_errno(Error), dwarf_errmsg(Error));
+    exit(-1);
+}
+
 int main(void)
 {
+    int DwarfResult = -1;
     int FileDescriptor;
-    Dwarf_Handler DwarfHandler;
-    Dwarf_Ptr DwarfErrArg;
+
+    Dwarf_Ptr DwarfErrArg = 0;
     Dwarf_Debug DwarfDebug;
     Dwarf_Error DwarfError;
 
     FileDescriptor = open("a.out", O_RDONLY);
 
-    int DwarfInitResult = dwarf_init(FileDescriptor, DW_DLC_READ, DwarfHandler, DwarfErrArg, &DwarfDebug, &DwarfError);
-    if (DwarfInitResult != DW_DLV_OK) {
-        fprintf(stderr, "dwarf_init() error.\n");
+    DwarfResult = dwarf_init(FileDescriptor, DW_DLC_READ, DwarfErrorHandler, DwarfErrArg, &DwarfDebug, &DwarfError);
+    if (DwarfResult != DW_DLV_OK) {
+        fprintf(stderr, "dwarf_init() error: %d\n", DwarfResult);
         exit(-1);
     }
 
-    int DwarfFinishResult = dwarf_finish(DwarfDebug, &DwarfError);
-    if (DwarfFinishResult != DW_DLV_OK) {
-        fprintf(stderr, "dwarf_init() error.\n");
+    fprintf(stdout, "DwarfDebug: %p\nDwarfError: %p\n", DwarfDebug, DwarfError);
+
+    DwarfResult = dwarf_finish(DwarfDebug, &DwarfError);
+    if (DwarfResult != DW_DLV_OK) {
+        fprintf(stderr, "dwarf_init() error: %d\n", DwarfResult);
         exit(-1);
     }
 
